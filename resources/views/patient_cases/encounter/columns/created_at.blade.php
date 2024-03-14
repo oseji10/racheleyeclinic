0d18f964a184ad0f04444484cbab3dca226f4f4a
voseji <vooseji@emgeresources.com>
2024-03-14 01:53:00 +0100

    new updates

--- resources/views/patient_cases/encounter/columns/created_at.blade.php
+++ resources/views/patient_cases/encounter/columns/created_at.blade.php
@@ -2,6 +2,10 @@
     @if (empty($row->created_at))
         {{ __('messages.common.n/a') }}
     @else
-        <span class="badge bg-light-success" style="color:black">{{ $row->created_at }}</span>
+    <?php $carbonDate = \Carbon\Carbon::parse($row->created_at); ?>
+    {{-- Format the date as "Monday 12th March, 2024" --}}
+    <span class="badge bg-light-success" style="color:black">{{ $carbonDate->format('l jS F, Y') }}</span>
+
+        {{-- <span class="badge bg-light-success" style="color:black">{{ $row->created_at }}</span> --}}
     @endif
 </div>
--- resources/views/patient_cases/encounter/columns/followup_appointment_date.blade.php
+++ resources/views/patient_cases/encounter/columns/followup_appointment_date.blade.php
@@ -2,6 +2,9 @@
     @if (empty($row->followup_appointment_date))
         {{ __('messages.common.n/a') }}
     @else
-        <span class="badge bg-light-warning" style="color:black">{{ $row->followup_appointment_date }}</span>
+        {{-- Convert the date to a Carbon instance --}}
+        <?php $carbonDate = \Carbon\Carbon::parse($row->followup_appointment_date); ?>
+        {{-- Format the date as "Monday 12th March, 2024" --}}
+        <span class="badge bg-light-warning" style="color:black">{{ $carbonDate->format('l jS F, Y') }} at {{ $carbonDate->format('h:i A') }}</span>
     @endif
 </div>
