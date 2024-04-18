495fcb6a5362aca8d37175fd4afd8f04f98f9acb
voseji <vooseji@emgeresources.com>
2024-04-18 21:49:33 +0100

    latest updates

--- resources/views/patient_cases/encounter/show_fields.blade.php
+++ resources/views/patient_cases/encounter/show_fields.blade.php
@@ -694,8 +694,12 @@
                             <label for="name"
                                 class="pb-2 fs-5 text-gray-600">{{ __('messages.case.free_handwriting_left') }}</label>
                             <p>
-                                <span
-                                class="fs-5 text-gray-800"><img src="{{ URL::asset($visual_acuity->free_handwriting_left) }}" />{{ URL::asset($visual_acuity->free_handwriting_left) }}</span>
+                                {{-- <span
+                                class="fs-5 text-gray-800"><img src="{{ URL::asset($visual_acuity->free_handwriting_left) }}" /></span> --}}
+                                @php
+    $imageUrl = "{{ URL::asset($visual_acuity->free_handwriting_left) }}";
+@endphp
+<img src="{{ $imageUrl }}" alt="Left Eye">
                             </p>
                         </div>
                    </div>
