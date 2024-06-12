80542c86ea168b2ab7eab3eec3f1b8d66f2debab
Victor Oseji <vctroseji@gmail.com>
2024-06-12 04:25:22 +0100

    latest

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -20,6 +20,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'razorpay-payment-success',
-        'ipd-razorpay-payment-success'
+        'ipd-razorpay-payment-success',
+        'livewire/*',
     ];
 }
